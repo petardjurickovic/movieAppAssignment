a86bc9d405243e08c5c5c0e0942e023b197cf0ce
Petar <petardjurickovic@gmail.com>
2023-04-14 02:14:48 +0200

    First version of the app

---
+++ movie3/ContentView.swift
@@ -0,0 +1,38 @@
+//
+//  ContentView.swift
+//  movie3
+//
+//  Created by Obuka on 5.4.23..
+//               
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        TabView {
+            DiscoverView()
+                .tabItem {
+                    Image(systemName: "popcorn")
+                }
+            SearchMoviesView()
+                .tabItem {
+                    Image(systemName: "magnifyingglass")
+                }
+            BookmarkedView()
+                .tabItem {
+                    Image(systemName: "bookmark.fill")
+                }
+        }.background(Color(red:39/255,green:40/255,blue:59/255))
+            .onAppear {
+                UITabBar.appearance().barTintColor = UIColor(red:39/255,green:40/255,blue:59/255, alpha: 1.0)
+            }
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ movie3/movie3App.swift
@@ -0,0 +1,17 @@
+//
+//  movie3App.swift
+//  movie3
+//
+//  Created by Obuka on 5.4.23..
+//
+
+import SwiftUI
+
+@main
+struct movie3App: App {
+    var body: some Scene {
+        WindowGroup {
+            SplashScreenView()
+        }
+    }
+}
